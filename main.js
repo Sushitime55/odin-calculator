7974b75a1b14135fe3de6162ec414a0b0bde76fb
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-18 03:57:48 -0700

    feat: operate function

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+  }
+}
+
 function add(x, y) {
   return x + y;
 }
 
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y == 0) return Error;
+
+  return x / y;
+}
+
 module.exports = {
+  operate,
   add,
+  subtract,
+  multiply,
+  divide,
 };
--- main.test.js
+++ main.test.js
@@ -1,8 +1,26 @@
 import { describe, test, it, expect } from "vitest";
 const calculator = require("./main");
 
+describe("operate", () => {
+  it("should call add when '+' is passed", () => {
+    expect(calculator.operate("+", 4, 9)).toBe(13);
+  });
+  it("should call divide when '/' is passed", () => {
+    expect(calculator.operate("/", 12, 6)).toBe(2);
+  });
+});
+
 describe("add", () => {
   it("should return sum of both values", () => {
     expect(calculator.add(5, 7)).toBe(12);
+  });
+});
+
+describe("divide", () => {
+  it("should return quotient of two values", () => {
+    expect(calculator.divide(8, 2)).toBe(4);
+  });
+  it("should return Error when dividing by zero", () => {
+    expect(calculator.divide(8, 0)).toBe(Error);
   });
 });
