4bd61c0fd5e8c8709e3ccc81ab10cf31b4fb9efc
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2025-09-18 22:20:18 -0700

    feat: add basic button functionality

--- main.js
+++ main.js
@@ -11,6 +11,31 @@ function operate(operator, x, y) {
   }
 }
 
+// store inputs ( x, operator, y)
+// once inputs are 3, call operate with inputs
+// inputs are stored on screen, so change screen somehow
+function onButtonPress(key) {
+  const screen = document.querySelector(".screen");
+  screen.textContent += key;
+  console.log(screen.textContent);
+
+  let input = screen.textContent;
+  if (input.length == 3) {
+    console.log(`commencing operate(${input[1]}, ${input[0]}, ${input[2]})`);
+    let result = operate(input[1], input[0], input[2]);
+    console.log(`result: ${result}`);
+    screen.textContent = "";
+  }
+}
+
+function addButtonFunctionality() {
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", onButtonPress.bind(event, button.textContent));
+  });
+}
+
+// MATH FUNCTIONS
 function add(x, y) {
   return x + y;
 }
@@ -29,8 +54,12 @@ function divide(x, y) {
   return x / y;
 }
 
+addButtonFunctionality();
+
 module.exports = {
   operate,
+  onButtonPress,
+  addButtonFunctionality,
   add,
   subtract,
   multiply,
